7f41d1e09bb99f291ba60f97b7ccb1b2fb5cd10a
rongqibei <156367878+rongqibei@users.noreply.github.com>
2024-04-04 16:04:46 -0400

    Delete challenge1.js

--- challenge1.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('subscribe').addEventListener('click', function(){
-    if(this.checked){
-        document.getElementById('emailDiv').style.display = 'block'
-    }
-    else {
-        document.getElementById('emailDiv').style.display = 'none'
-    }
-})
